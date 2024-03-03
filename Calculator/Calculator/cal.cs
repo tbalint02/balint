0abcfbce7b2af685669f308b3015c8fd6f847da4
tbalint02 <160844349+tbalint02@users.noreply.github.com>
2024-03-03 11:48:34 +0100

    Add files via upload

---
+++ Calculator/Calculator/cal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(int a, int b) { return a + b; }
+        public double Sub(int a, int b) { return a - b; }
+        public double Mul(int a, int b) { return a * b; }
+        public double Div(int a, int b) { return a / b; }
+    }
+}
