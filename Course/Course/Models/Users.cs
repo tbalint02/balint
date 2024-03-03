1e51a3d1a025c9bb6a14d24e302fa6a21aa57f4b
tbalint02 <160844349+tbalint02@users.noreply.github.com>
2024-03-03 14:19:04 +0100

    Add files via upload

---
+++ Course/Course/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Models
+{
+    public class User
+    {
+        [Required]
+        public int? Id { get; set; }
+        [StringLength(10)]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }   
+
+
+    }
+}
---
+++ Course/Course/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Course.Models;
+
+namespace Course.Repositories
+{
+    public class UserRepository
+    {
+        private static List<User> users = new List<User> { new User { Id = 1, FirstName = "john", LastName = "Nagy" } };
+        public List<User> GetAll() { return users; }
+
+        public User? GetById(int id)
+        {
+            foreach (var user in users)
+            {
+                if (user.Id == id)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+        public void Create(User data)
+        {
+            users.Add(data);
+        }
+        public User? Update(int id, User data)
+        {
+            foreach(var user in users)
+            {
+                if (user.Id == id)
+                {
+                    user.FirstName = data.FirstName;
+                    user.LastName = data.LastName;
+                    return user;
+
+                }
+            }
+            return null;
+        }
+        public bool delete(int id)
+        {
+            foreach(var user in users)
+            {
+                if (user.Id == id)
+                {
+                    users.Remove(user);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
